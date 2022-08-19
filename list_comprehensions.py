bfe804f90ca6201b08ae8354f8958b83580a952f
Gaby Vazquez <gaby27anahi02@outlook.es>
2022-08-19 15:23:49 -0500

    commit 4

---
+++ list_comprehensions.py
@@ -0,0 +1,2 @@
+def run():
+    my_dict = {}
--- lists_and_dicts.py
+++ lists_and_dicts.py
@@ -1,5 +1,25 @@
 def run():
-    pass
+    my_list = [1, "Hello", True, 4.5]
+    my_dict = {"firstname":"Gabriela", "lastname": "Vazquez"}
+
+    super_list = [
+         {"firstname": "Gabriela", "lastname": "Vazquez"},
+        {"firstname": "Ruth", "lastname": "Parraguirre"},
+        {"firstname": "Miguel", "lastname": "Serrano"},
+        {"firstname": "Luis", "lastname": "Nava"},
+        {"firstname": "Gerardo", "lastname": "Enriquez"},
+    ] 
+       
+    
+
+    super_dict = {
+        "natural_nums": [1, 2, 3, 4, 5],
+        "integer_nums": [-1, -2, 0, 1, 2],
+        "floating_nums":[1.1, 4.5, 6.8]
+    }
+
+    for key, value in super_dict.items():
+        print(key, "-", value)
 
 
 if __name__ == "_main_":
