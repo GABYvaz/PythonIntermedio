97c4744421916ce3778cb9241a2da06e52d1d830
Gaby Vazquez <gaby27anahi02@outlook.es>
2022-08-19 16:22:03 -0500

    commit 7

---
+++ filtrando_datos.py
@@ -0,0 +1,12 @@
+def run():
+    pass
+
+
+
+     def run():
+        all_python_devs = [worker["name"]for worker in DATA if worker{"language"} == "python"]
+        for worker in all_python_devs:
+            print(worker)
+
+          if _name_ == "_main_": 
+            run() 
